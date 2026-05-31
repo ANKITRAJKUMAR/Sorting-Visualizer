c9cc0ce07fb06ab2175e6576e929ebb05b5058bb
Ankit Raj <rajankit20020@gmail.com>
2026-05-31 08:53:37 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+
+server.listen(1337);
+
+app.use(express.static(path.join(__dirname, "..", "client")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "index.html"));
+});
